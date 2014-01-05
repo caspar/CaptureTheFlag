be68c233a9718786c7799fd7a1420a83b21de89c
Spencer Weiss <spencerc.weiss@gmail.com>
2014-01-05 17:20:25 -0500

    Tossed in my two cents

--- FlagGame.java
+++ FlagGame.java
@@ -10,6 +10,10 @@ public class FlagGame {
   
     //we need some way to store things like highscores, statistics, playernames, saved games, etc. 
     //any ideas? - C
+    
+    // text files should do the trick. Players should input a username and password if they want to have their
+    // highscores, stats, etc. saved, otherwise they will just be relocated to a temporary file that will be
+    // deleted every time they quit. Sound good? -SW 
 
     public static void main(String[] args){
 	Gui G = new Gui();
