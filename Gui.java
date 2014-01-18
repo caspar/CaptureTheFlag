06a67b3a48758ca9488c42b35ac45a3ee9e0f04f
Spencer Weiss <spencerc.weiss@gmail.com>
2014-01-18 18:12:39 -0500

    Random buttons work
    Fixed more than just a semicolon issue

--- Gui.java
+++ Gui.java
@@ -8,6 +8,8 @@ import javax.imageio.*;
 
 public class Gui  extends JFrame implements ActionListener {
  
+    
+ 
     Game Game = new Game();
     Random Rand = new Random();
 
@@ -21,10 +23,17 @@ public class Gui  extends JFrame implements ActionListener {
     private JPanel flagpanel;
 
     ///// For newButtons() /////
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
+    private String[] abcd = {"A. ","B. ","C. ","D. "};
+    private String b1 = abcd[0];
+    private String b2 = abcd[1];
+    private String b3 = abcd[2];
+    private String b4 = abcd[3];
+    private String[] choices = {b1, b2, b3, b4};
+    JButton button1 = new JButton(b1);
+    JButton button2 = new JButton(b2);
+    JButton button3 = new JButton(b3);
+    JButton button4 = new JButton(b4);
+    private JButton[] answers = {button1, button2, button3, button4};
 
     
     public Gui() { //CASPAR
@@ -58,80 +67,19 @@ public class Gui  extends JFrame implements ActionListener {
 	return flagLabel;
     }
        
-    
-    private void buttonAnswers() {  // creates the buttons
-    
-        String[] abcd = {"A","B","C","D"};
-        answers = new JButton[4];
-        
-        // {"" "" "" ""}
-        
-        for (int i = 0; i < 4; i++) {
-            answers[i] = new JButton(abcd[i]); // {A B C D}
-            answers[i].addActionListener(this);
-        }
-    }
-    
     public void assignAnswersToButtons(String[] answers) {
         
-        int r = new Random (4); // the index of the button to be given the correct answer
+        Random rand = new Random();
+        int r = rand.nextInt(4); // the index of the button to be given the correct answer
 
-	for (int i = 0; i < 4; i++) {
-            buttonAnswers[i].setText(answers[i] + getName(Game.flagList).get(Rand.nextInt(233))); // assigns random country
-        }    
+        for (int i = 0; i < 4; i++) {
+            choices [i] += getName((Game.flagList).get(Rand.nextInt(233))); // assigns random country to the buttons
+            answers[i] = choices[i];
+        }
 
-        JButton correctAnswer = answers[r]; // reassigns the correct answer to the random button
-        
-        
-        frame.setSize(1280, 800);
-        frame.setBackground(new Color(30,30,30));
-        frame.setMinimumSize(new Dimension(800, 600));
-        frame.setVisible(true);
-        
+        choices[r] = abcd[r] + "path of answer"; // reassigns the correct answer to the random button
+        answers[r] = choices[r];
     }
-
-
-    
-    /* private void newButtons(String correctName){ //SPENCER
-	//must create 4 unique buttons below flag, one of which is correct.
-	//the correct answer should not be in the same place every time. 
-	//must be large enough to accomodate large country names (36 characters) --> so 40's good
-	//should call getName() in each button init.
-	//Like so:
-	
-	frame = new JFrame("Flag Game");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setLayout(new MenuLayout());
-	
-	String button1 = getName(Game.flagList).get(Rand.nextInt(233)));
-	String button2 = getName(Game.flagList).get(Rand.nextInt(233)));
-	String button3 = getName(Game.flagList).get(Rand.nextInt(233)));
-	String button4 = getName(Game.flagList).get(Rand.nextInt(233)))
-	JButton[] buttons = new JButton(4);  // array of buttons
-	buttons[0] = new JButton(""); // the following adds four blank buttons into it
-	buttons[1] = new JButton("");
-	buttons[2] = new JButton("");
-	buttons[3] = new JButton("");
-	Random r = new Random(4); 
-	buttons[r] = 
-	//String rand = r.getName(Game.flagList).get(Rand.nextInt(233));
-	//buttons[rand] = buttons[rand].setText("" + "Whatever the path is.");
-	// This didn't do the trick at all lol
-	
-	button1.addActionListener(this);
-        button2.addActionListener(this);
-        button3.addActionListener(this);
-        button4.addActionListener(this);
-
-        frame.getContentPane().add(button1);
-        frame.getContentPane().add(button2);
-        frame.getContentPane().add(button3);
-        frame.getContentPane().add(button4);
-
-
-	
-    }
-    */
     
     
     public int longestCountry(){ //CASPAR
