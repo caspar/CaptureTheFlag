d1a5f6c143820c2fdb49f9ca313cb3bcb15d1742
Spencer Weiss <spencerc.weiss@gmail.com>
2014-01-25 23:24:50 -0500

    Added multiple methods - Spencer

--- StatsGui.java
+++ StatsGui.java
@@ -6,19 +6,58 @@ public class StatsGui {
 	
 	public boolean isRacist = true; // guilty until proven innocent
 	public int score = 0;
+	public int ameurocaScore = 0;
+	public int ameurocaGuesses = 0;
+	public int africasiaScore = 0;
+	public int africasiaGuesses = 0;
 	public int numGuesses = 0;
 	public int currentStreak = 0;
 	public int longestStreak = 0;
-	public double guessPercentage = 0.0;
+	public double overallGuessPercentage = 0.0;
+	public double ameurocaPercentage = 0.0;
+	public double africasiaPercentage = 0.0;
 	
-	public void getPercentage() {
-		guessPercentage = score / numGuesses
+	public void updateBasicStats(boolean input) {
+		numGuesses += 1
+		if (input = true) {
+			score += 1;
+			currentStreak += 1;
+			if (currentStreak >= longestStreak) {
+				longestStreak += 1;
+			}
+		}
 	}
 
-//	public boolean racistCheck() {
-	//	return (europeScore + americaScore > otherScore);
-		// in another file, if this is true, the message is "I think you're a racist >:("
-//	}
+	public void updateSpecificStats(boolean input, String continent) {
+		if (continent == America || continent == Europe) {
+			ameurocaGuesses += 1;
+			if (input == true) {
+				ameurocaScore += 1;
+			}
+		}
+		else {
+			africasiaGuesses += 1;
+			if (input == true) {
+				africasiaScore += 1;
+			}
+		}
+	}
+	
+	public void getOverallPercentage() {
+		overallGuessPercentage = score / numGuesses;
+	}
+	
+	public void getAmeurocaPercentage() {
+		ameurocaPercentage = ameurocaScore / ameurocaGuesses;
+	}
+	
+	public void getAfricasiaPercentage() {
+		africasiaPercentage = africasiaScore / africasiaGuesses;
+	}
+
+	public boolean racistCheck() {
+		return ((ameurocaPercentage - africasiaPercentage >= 15.0));
+	}
 
 	public void saveStats() {
 		// code that saves stats
