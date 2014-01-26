99aad06617d475249e2fd6a1e11a0ad7ebbf8ac7
Spencer Weiss <spencerc.weiss@gmail.com>
2014-01-25 23:03:29 -0500

    Basic changes -S

--- StatsGui.java
+++ StatsGui.java
@@ -5,9 +5,15 @@ import java.util.*;
 public class StatsGui {
 	
 	public boolean isRacist = true; // guilty until proven innocent
-	public int numRight = 0;
-	public int streak = 0;
+	public int score = 0;
+	public int numGuesses = 0;
+	public int currentStreak = 0;
 	public int longestStreak = 0;
+	public double guessPercentage = 0.0;
+	
+	public void getPercentage() {
+		guessPercentage = score / numGuesses
+	}
 
 //	public boolean racistCheck() {
 	//	return (europeScore + americaScore > otherScore);
