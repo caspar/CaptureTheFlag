1c6cfec69000154c94ef2ba7e78ca7600b0693f0
Spencer Weiss <spencerc.weiss@gmail.com>
2014-01-26 12:26:08 -0500

    Factored in the fifth category -Spencer

--- StatsGui.java
+++ StatsGui.java
@@ -8,14 +8,14 @@ public class StatsGui {
 	public int score = 0;
 	public int ameurocaScore = 0;
 	public int ameurocaGuesses = 0;
-	public int africasiaScore = 0;
-	public int africasiaGuesses = 0;
+	public int oceafricasiaScore = 0;
+	public int oceafricasiaGuesses = 0;
 	public int numGuesses = 0;
 	public int currentStreak = 0;
 	public int longestStreak = 0;
 	public double overallGuessPercentage = 0.0;
 	public double ameurocaPercentage = 0.0;
-	public double africasiaPercentage = 0.0;
+	public double oceafricasiaPercentage = 0.0;
 	
 	public void updateBasicStats(boolean input) {
 		numGuesses += 1
@@ -36,9 +36,9 @@ public class StatsGui {
 			}
 		}
 		else {
-			africasiaGuesses += 1;
+			oceafricasiaGuesses += 1;
 			if (input == true) {
-				africasiaScore += 1;
+				oceafricasiaScore += 1;
 			}
 		}
 	}
@@ -51,21 +51,23 @@ public class StatsGui {
 		ameurocaPercentage = ameurocaScore / ameurocaGuesses;
 	}
 	
-	public void getAfricasiaPercentage() {
-		africasiaPercentage = africasiaScore / africasiaGuesses;
+	public void getOceafricasiaPercentage() {
+		oceafricasiaPercentage = oceafricasiaScore / oceafricasiaGuesses;
 	}
 
 	public boolean racistCheck() {
-		return ((ameurocaPercentage - africasiaPercentage >= 15.0));
+		return ((ameurocaPercentage - oceafricasiaPercentage >= 15.0));
 	}
 
 	public void saveStats() {
 		// code that saves stats
+		// do we really need this?
 	}
 
 
 	public void writeStats() {
 		// code that writes changes to stats.txt
+		// this also seems unnecessary
 	}
 
 }
